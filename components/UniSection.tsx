b53e298cfdcda8d01c424b5bc8b151270c4e3933
Cody Philipp <codyphilipp@gmail.com>
2023-09-16 16:11:06 +1000

    Added full functionality to UniSection:     - Can add extra Uni entries     - Can delete Uni entries     - Can add Uni degrees and name them     - Can delete Uni degrees

--- components/UniSection.tsx
+++ components/UniSection.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 const uwaInfo = {
+  id: 0,
   title: "University of Western Australia",
   type: "Bachelor of Science & Engineering",
   degrees: ["Astrophysics", "Mechanical Engineering"],
@@ -8,6 +9,7 @@ const uwaInfo = {
   endYear: "2019",
 };
 const umInfo = {
+  id: 1,
   title: "University of Melbourne",
   type: "Master of Engineering",
   degrees: ["Aerospace Engineering"],
@@ -19,12 +21,95 @@ interface UniSectionProps {
   isEditing: boolean;
 }
 
+type UniEntry = {
+    id: number,
+    title: string,
+    type: string,
+    degrees: string[],
+    startYear: string,
+    endYear: string,
+}
+
 const UniSection: React.FC<UniSectionProps> = ({ isEditing }) => {
   const [uniInfo, setUniInfo] = useState([uwaInfo, umInfo]);
 
 
-  function degreeHandleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    
+  function handleUniInfoChange(e: React.ChangeEvent<HTMLInputElement>, entryId: number) {
+    setUniInfo(uniInfo.map(uni => {
+        if (entryId === uni.id) {
+            return {
+                ...uni,
+                [e.target.name]: e.target.value
+            }
+        } else {
+            return uni
+        }
+    }))
+  }
+
+  function handleAddEntryClick() {
+    const defaultEntry: UniEntry = {
+        id: uniInfo.length,
+        title: "",
+        type: "",
+        degrees: [""],
+        startYear: "",
+        endYear: ""
+    }
+    setUniInfo([...uniInfo, defaultEntry])
+  }
+
+  function handleAddDegreeClick(entryId: number) {
+    setUniInfo(uniInfo.map(uni => {
+        if (entryId === uni.id) {
+            return {
+                ...uni,
+                degrees: [...uni.degrees, ""]
+            }
+        } else {
+            return uni
+        }
+    }))
+  }
+
+  function handleDegreeChange(e: React.ChangeEvent<HTMLInputElement>, entryId: number, index: number) {
+    setUniInfo(uniInfo.map(uni => {
+        if (entryId === uni.id) {
+            const nextDegreeList = uni.degrees.map((deg, i) => {
+                if (i === index) {
+                    return e.target.value
+                } else {
+                    return deg
+                }
+            })
+            return {
+                ...uni,
+                degrees: [...nextDegreeList]
+            }
+        } else {
+            return uni
+        }
+    }))
+  }
+
+  function handleDeleteDegreeClick(entryId: number, index: number) {
+    setUniInfo(uniInfo.map(uni => {
+        if (entryId === uni.id) {
+            const nextDeletedIndexList = uni.degrees.filter((ud, i) => i !== index)
+            return {
+                ...uni,
+                degrees: [...nextDeletedIndexList]
+            }
+        } else {
+            return uni
+        }
+    })
+    )
+  }
+
+  function handleDeleteEntryClick(idToDelete: number) {
+    const deletedIdList = uniInfo.filter(uI => uI.id !== idToDelete)
+    setUniInfo([...deletedIdList])
   }
 
 
@@ -39,45 +124,62 @@ const UniSection: React.FC<UniSectionProps> = ({ isEditing }) => {
                 <div className="flex justify-between items-center">
                   <div className="px-6 text-slate-600 space-y-2">
                     <input
+                      name="title"
                       className="w-[44%]"
                       placeholder="University name"
                       value={uni.title}
+                      onChange={e => handleUniInfoChange(e, uni.id)}
                     />
                     <div className="space-y-2">
                       <div className="flex space-x-2">
                         <input
+                          name="type"
                           className="w-[44%]"
                           placeholder="Degree Type"
                           value={uni.type}
+                          onChange={e => handleUniInfoChange(e, uni.id)}
                         />
                         <input
+                            name="startYear"
                           className="w-[10%] text-center"
                           placeholder="Year Start"
                           value={uni.startYear}
+                          onChange={e => handleUniInfoChange(e, uni.id)}
                         />
                         <input
+                            name="endYear"
                           className="w-[10%] text-center"
                           placeholder="Year End"
                           value={uni.endYear}
+                          onChange={e => handleUniInfoChange(e, uni.id)}
                         />
                       </div>
 
                       {/* Mapping out each major */}
                       <ol className="px-6 space-y-2">
-                        {uni.degrees.map((deg) => (
-                          <li className="list-disc" key={deg}>
-                            <input placeholder="Major Title" value={deg}/>
+                        {uni.degrees.map((deg, index) => (
+                        <div className="flex items-center gap-x-2">
+                          <li key={index} className="list-disc">
+                            <input name="degrees" placeholder="Major Title" value={deg}
+                             onChange={(e) => {handleDegreeChange(e, uni.id, index)}}/>
                           </li>
+                          <button className="bg-[#F19A3E] w-4 h-4 rounded-full hover:scale-[110%] flex items-center justify-center text-[15px] pb-[0px] text-white"
+                            onClick={() => {handleDeleteDegreeClick(uni.id, index)}}>
+                            -
+                        </button>
+                        </div>
                         ))}
                         <li className="list-disc" >
-                        <button className="bg-[#F19A3E] w-4 h-4 rounded-full hover:scale-[110%] flex items-center justify-center text-[15px] pb-[0px] text-white">
+                        <button className="bg-[#F19A3E] w-4 h-4 rounded-full hover:scale-[110%] flex items-center justify-center text-[15px] pb-[0px] text-white"
+                         onClick={() => handleAddDegreeClick(uni.id)}>
                             +
                         </button>
                         </li>
                       </ol>
                     </div>
                   </div>
-                  <button className="bg-[#F19A3E] w-8 h-8 rounded-full hover:scale-[110%] flex items-center justify-center text-[30px] pb-1 text-white">
+                  <button className="bg-[#F19A3E] w-8 h-8 rounded-full hover:scale-[110%] flex items-center justify-center text-[30px] pb-1 text-white"
+                    onClick={() => handleDeleteEntryClick(uni.id)}>
                     -
                   </button>
                 </div>
@@ -87,7 +189,8 @@ const UniSection: React.FC<UniSectionProps> = ({ isEditing }) => {
           })}
 
           <div className="flex justify-center">
-            <button className="bg-[#F19A3E] w-12 h-12 rounded-full hover:scale-[110%] flex items-center justify-center text-[40px] pb-1">
+            <button className="bg-[#F19A3E] w-12 h-12 rounded-full hover:scale-[110%] flex items-center justify-center text-[40px] pb-1"
+                onClick={handleAddEntryClick}>
               +
             </button>
           </div>
